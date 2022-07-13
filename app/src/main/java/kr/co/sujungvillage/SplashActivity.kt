4548e93c65cdc98d6b3537da915e23300f635732
cloudwoon <cloud.wooo0n@gmail.com>
2022-07-13 23:52:25 +0900

    [FEAT/#3] Splash 완성

--- app/src/main/java/kr/co/sujungvillage/SplashActivity.kt
+++ app/src/main/java/kr/co/sujungvillage/SplashActivity.kt
@@ -1,11 +1,35 @@
 package kr.co.sujungvillage
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        loadSplashScreen()
+    }
+    private fun loadSplashScreen() {
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4600)
     }
 }
