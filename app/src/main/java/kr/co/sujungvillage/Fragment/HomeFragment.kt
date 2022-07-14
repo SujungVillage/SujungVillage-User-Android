5363ccf209c18cbd0d1c7307eee759e0fd5f9fcf
flora7883 <flora7883@gmail.com>
2022-07-14 23:46:57 +0900

    [MERGE]menifest 수정

--- app/src/main/java/kr/co/sujungvillage/Fragment/HomeFragment.kt
+++ app/src/main/java/kr/co/sujungvillage/Fragment/HomeFragment.kt
@@ -1,22 +1,26 @@
 package kr.co.sujungvillage.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.co.sujungvillage.R
+import kr.co.sujungvillage.StayoutActivity
+import kr.co.sujungvillage.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        // 외박 신청 버튼 연결
+        binding.btnStayout.setOnClickListener {
+            var intent = Intent(this.activity, StayoutActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
     }
 }
