781a9180aedac820cb637c532f170eec02250a4e
b1urrrr <b1urrr@naver.com>
2022-08-02 02:00:56 +0900

    [FEAT/#12] 외박 신청 API 연결 시도

--- app/src/main/java/kr/co/sujungvillage/data/Stayout.kt
+++ app/src/main/java/kr/co/sujungvillage/data/Stayout.kt
@@ -2,6 +2,7 @@ package kr.co.sujungvillage.data
 
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+import java.util.*
 
 data class StayoutCreateDTO(
     @SerializedName("destination")
@@ -14,9 +15,41 @@ data class StayoutCreateDTO(
     val date: String,
 ): Serializable {}
 
-data class StayoutCodeDTO(
-    @SerializedName("code")
-    val code: Long,
-    @SerializedName("data")
-    val data: String,
+data class StayoutCreateResultDTO(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("user")
+    val user: StayoutUser,
+    @SerializedName("destination")
+    val destination: String,
+    @SerializedName("reason")
+    val reason: String,
+    @SerializedName("emergencyPhoneNumber")
+    val emergencyNumber: String,
+    @SerializedName("dateToUse")
+    val date: Date,
 ): Serializable {}
+
+data class StayoutUser(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("phoneNumber")
+    val phoneNumber: String,
+    @SerializedName("domitory")
+    val dormitory: StayoutDormitory,
+    @SerializedName("detailedAddress")
+    val address: String,
+    @SerializedName("authority")
+    val authority: String,
+): Serializable {}
+
+data class StayoutDormitory(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("dormitoryName")
+    val name: String,
+    @SerializedName("address")
+    val address: String,
+):Serializable {}
