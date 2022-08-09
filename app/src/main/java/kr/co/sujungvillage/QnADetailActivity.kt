494ddbca40f71a7cd9f3b8f29bff1af5b38a6248
b1urrrr <b1urrr@naver.com>
2022-08-09 22:26:02 +0900

    [FEAT/#91] QnA Detail 레이아웃 구현

--- app/src/main/java/kr/co/sujungvillage/QnADetailActivity.kt
+++ app/src/main/java/kr/co/sujungvillage/QnADetailActivity.kt
@@ -2,15 +2,14 @@ package kr.co.sujungvillage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.co.sujungvillage.databinding.ActivityAlarmBinding
+import kr.co.sujungvillage.databinding.ActivityQnAdetailBinding
 
 
 class QnADetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAlarmBinding
+    val binding by lazy { ActivityQnAdetailBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // 뒤로가기 버튼 연결
