1ed43084d665204ea9c2ba582dace7d0a61765e7
hyunjiLee123 <bm4bw00@naver.com>
2022-08-09 15:42:45 +0900

    [MDO/#5]qna 8/9 모각코

--- app/src/main/java/kr/co/sujungvillage/QnADetailActivity.kt
+++ app/src/main/java/kr/co/sujungvillage/QnADetailActivity.kt
@@ -1,16 +1,28 @@
 package kr.co.sujungvillage
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import kr.co.sujungvillage.adapter.ViewPagerAdapter
+import kr.co.sujungvillage.databinding.ActivityAlarmBinding
 import kr.co.sujungvillage.databinding.ActivityMainBinding
 
 
 class QnADetailActivity : AppCompatActivity() {
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var binding: ActivityAlarmBinding
+    private val tabTitleArray = arrayOf(
+        "FAQ",
+        "내질문"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 뒤로가기 버튼 연결
+        binding.btnBack.setOnClickListener { finish() }
 
     }
 }
