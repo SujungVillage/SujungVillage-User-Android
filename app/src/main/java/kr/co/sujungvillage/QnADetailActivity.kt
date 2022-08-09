d7dfc01d203d208a54582bf213bea4215a3b6d8e
hyunjiLee123 <bm4bw00@naver.com>
2022-08-09 14:58:35 +0900

    [MOD/#5]qna 파일 수정 중간

--- app/src/main/java/kr/co/sujungvillage/QnADetailActivity.kt
+++ app/src/main/java/kr/co/sujungvillage/QnADetailActivity.kt
@@ -2,10 +2,15 @@ package kr.co.sujungvillage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.sujungvillage.databinding.ActivityMainBinding
+
 
 class QnADetailActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qn_adetail)
+        setContentView(binding.root)
+
     }
 }
