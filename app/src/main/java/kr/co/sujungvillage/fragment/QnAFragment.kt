26401ffe0170b8f9a0c818a1d771a276f2f52ca9
b1urrrr <b1urrr@naver.com>
2022-08-09 20:17:42 +0900

    [FEAT/#5] QnA 레이아웃 구현

--- app/src/main/java/kr/co/sujungvillage/fragment/QnAFragment.kt
+++ app/src/main/java/kr/co/sujungvillage/fragment/QnAFragment.kt
@@ -1,19 +1,36 @@
 package kr.co.sujungvillage.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kr.co.sujungvillage.R
+import com.google.android.material.tabs.TabLayoutMediator
+import kr.co.sujungvillage.adapter.QnAPagerAdapter
+import kr.co.sujungvillage.databinding.ActivityAlarmBinding
+import kr.co.sujungvillage.databinding.FragmentQnABinding
 
 class QnAFragment : Fragment() {
+    private val tabTitleArray = arrayOf("FAQ", "내 질문")
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qn_a, container, false)
+    val binding by lazy { FragmentQnABinding.inflate(layoutInflater) }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentQnABinding.inflate(inflater, container, false)
+
+        // 프래그먼트 전환 상황을 저장하지 않음 (아니면 오류 발생)
+        binding.viewPager.isSaveEnabled = false
+
+        // viewPager와 tabLayout 설정이 되어 있지 않은 경우 설정
+        if (binding.viewPager.adapter == null) {
+            binding.viewPager.adapter = this.activity?.let { QnAPagerAdapter(childFragmentManager, lifecycle) }
+
+            TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+                tab.text = tabTitleArray[position]
+            }.attach()
+        }
+
+        return binding.root
     }
 }
