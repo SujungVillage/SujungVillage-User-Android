9b58be50165666276a0d199586c6728979871b5a
flora7883 <flora7883@gmail.com>
2022-08-13 03:59:06 +0900

    [REFACTOR/#4] 커뮤니티 글, 댓글 삭제 기능 api 연결

--- app/src/main/java/kr/co/sujungvillage/adapter/CommDetailAdapter.kt
+++ app/src/main/java/kr/co/sujungvillage/adapter/CommDetailAdapter.kt
@@ -1,19 +1,29 @@
 package kr.co.sujungvillage.adapter
 
+import android.content.Context
+import android.content.DialogInterface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import kr.co.sujungvillage.CommDetailActivity.Companion.studentNum
+import kr.co.sujungvillage.CommDetailActivity.Companion.token
 import kr.co.sujungvillage.base.hideKeyboard
 import kr.co.sujungvillage.data.CommDetailCommentsRequest
 import kr.co.sujungvillage.databinding.ListitemCommDetailBinding
+import kr.co.sujungvillage.retrofit.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class CommDetailAdapter() :RecyclerView.Adapter<CommDetailHolder>(){
+class CommDetailAdapter(val context:Context) :RecyclerView.Adapter<CommDetailHolder>(){
     var commDetailList=mutableListOf<CommDetailCommentsRequest>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommDetailHolder {
         val binding=ListitemCommDetailBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return CommDetailHolder(binding)
+        return CommDetailHolder(binding,context)
     }
 
     override fun onBindViewHolder(holder: CommDetailHolder, position: Int) {
@@ -26,7 +36,7 @@ class CommDetailAdapter() :RecyclerView.Adapter<CommDetailHolder>(){
     }
 
 }
-class CommDetailHolder(val binding:ListitemCommDetailBinding): RecyclerView.ViewHolder(binding.root){
+class CommDetailHolder(val binding:ListitemCommDetailBinding,val context: Context): RecyclerView.ViewHolder(binding.root){
     fun setCommDetail(commDetail: CommDetailCommentsRequest){
         binding.textName.text="익명" // 익명 처리해야함.
         val hour=commDetail.regDate?.subSequence(11,13).toString().toInt()
@@ -39,5 +49,32 @@ class CommDetailHolder(val binding:ListitemCommDetailBinding): RecyclerView.View
             binding.textAdmin.visibility= View.VISIBLE
         }
         binding.root.setOnClickListener { binding.root.context.hideKeyboard(itemView) }
+        //댓글 삭제 버튼
+        if(commDetail.writerId==studentNum) {
+            binding.textCommentDelete.visibility=View.VISIBLE
+        }
+
+        binding.textCommentDelete.setOnClickListener{
+            //경고창 띄우기
+            val builder= AlertDialog.Builder(context)
+            builder.setTitle("정말 삭제하시겠습니까?")
+                .setPositiveButton("확인", DialogInterface.OnClickListener{ dialog, id->
+                    RetrofitBuilder.communityApi.commCommentDelete(token,commDetail.id).enqueue(object :
+                        Callback<Void> {
+                        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                            Log.d("COMM_DELETE",response.message().toString())
+                            //리프레시 필요.
+                        }
+                        override fun onFailure(call: Call<Void>, t: Throwable) {
+                            Log.d("COMM_DELETE",t.message.toString())
+                        }
+                    })
+                })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener{ dialog, id->
+                        Log.d("COMM_DELETE","글 삭제 취소")
+                    })
+            builder.show()
+        }
     }
 }
