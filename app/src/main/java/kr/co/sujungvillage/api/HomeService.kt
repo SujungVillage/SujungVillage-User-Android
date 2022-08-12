80c3a28a569e7ddd3e5ef6ff3d5077c383720377
b1urrrr <b1urrr@naver.com>
2022-08-12 16:32:15 +0900

    [REFACTOR/#107] 로그인API 연결 및 토큰 저장

--- app/src/main/java/kr/co/sujungvillage/api/HomeService.kt
+++ app/src/main/java/kr/co/sujungvillage/api/HomeService.kt
@@ -6,9 +6,9 @@ import retrofit2.http.*
 
 interface HomeService {
     // 학생 홈 화면 정보 조회
-    @GET("/api/student/home_info")
+    @GET("/api/student/home/getInfo")
     fun homeInfo(
-        @Header("user_id") userId: String,
+        @Header("jwt_token") userId: String,
         @Query("year") year: String,
         @Query("month") month: String,
     ): Call<HomeInfoResultDTO>
