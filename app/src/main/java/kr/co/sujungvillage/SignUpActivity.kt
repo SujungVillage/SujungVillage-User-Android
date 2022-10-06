c04a6aadaf0e301dc28cde845ff03a1fd4ec767b
flora7883 <flora7883@gmail.com>
2022-10-06 17:13:56 +0900

    [FEAT/#145]회원가입 레이아웃

---
+++ app/src/main/java/kr/co/sujungvillage/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.sujungvillage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        //스피너 커스텀
+        //중복, 오류 처리 꼼꼼하게
+        //글자수 제한
+    }
+}
